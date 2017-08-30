48f6dd205aa6b9cca2869c37ae48ad9304ce7fb2
Riza Fahmi <rizafahmi@gmail.com>
2017-08-30 16:04:56 +0700

    Cache App Shell with ServiceWorker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,44 @@
-console.log('[ServiceWorker] Service workers kicks in!')
+/* global self, caches, fetch */
+
+const cacheName = 'misterpwa'
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/main.css',
+  '/styles/spinner.css',
+  '/images/logo.svg'
+]
+
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install')
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('[ServiceWorker] Caching the app shell')
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+self.addEventListener('activate', e => {
+  console.log('[ServiceWorker] Activated')
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  )
+})
